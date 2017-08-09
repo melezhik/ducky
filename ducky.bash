24ffe546395e4450d086e2ea09839afbde11cc82
Alexey Melezhik <melezhik@gmail.com>
2017-08-09 14:34:50 +0300

    update

--- ducky.bash
+++ ducky.bash
@@ -7,11 +7,14 @@ fi
 echo run ducky from $duckyfile ...
 
 if docker ps -f name=$1 | grep -q -w $1; then
+  echo use docker container $1 ...
   docker exec $1 \
   bash -c "curl -k -L -o - https://sparrowhub.org/bootstrap.sh | \
   bash && sparrow index update && sparrow box run $duckyfile"
 else
-  docker run  -v $PWD:/var/ducky -it $1 \
+  echo run docker container from $1 ...
+  #set -x
+  docker run  -v $PWD:/var/ducky -it --entrypoint="" $1 \
   bash -c "curl -k -L -o - https://sparrowhub.org/bootstrap.sh | \
   bash && sparrow index update && sparrow box run $duckyfile"
 fi
