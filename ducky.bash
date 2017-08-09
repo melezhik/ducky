dcbd7cb55513de46c2fefd2379b7a1359547c68b
Alexey Melezhik <melezhik@gmail.com>
2017-08-09 15:37:26 +0300

    bug fix

--- ducky.bash
+++ ducky.bash
@@ -1,7 +1,7 @@
-if test -z $DUCKYFILE;
-  duckyfile=/var/ducky/ducky.json
-then
-  duckyfile="/var/ducky/${DUCKYFILE}"
+if test -z $DUCKYFILE; then
+  duckyfile="/var/ducky/ducky.json"
+else
+  duckyfile="/var/ducky/$DUCKYFILE"
 fi
 
 echo run ducky from $duckyfile ...
@@ -9,13 +9,13 @@ echo run ducky from $duckyfile ...
 if docker ps -f name=$1 | grep -q -w $1; then
   echo use docker container $1 ...
   docker exec $1 \
-  bash -c "curl -k -L -o - https://sparrowhub.org/bootstrap.sh | \
+  bash -c "curl -s -k -L -o - https://sparrowhub.org/bootstrap.sh | \
   bash && sparrow index update && sparrow box run $duckyfile"
 else
   echo run docker container from $1 ...
   #set -x
   docker run  -v $PWD:/var/ducky -it --entrypoint="" $1 \
-  bash -c "curl -k -L -o - https://sparrowhub.org/bootstrap.sh | \
+  bash -c "curl -s -k -L -o - https://sparrowhub.org/bootstrap.sh | \
   bash && sparrow index update && sparrow box run $duckyfile"
 fi
 
