b27e0cf3de1034d82f2f7833599e98dc2a10ee9f
Alexey Melezhik <melezhik@gmail.com>
2017-08-14 11:49:52 +0300

    set enn

--- ducky.bash
+++ ducky.bash
@@ -8,13 +8,13 @@ echo run ducky from $duckyfile ...
 
 if docker ps -f name=$1 | grep -q -w $1; then
   echo use docker container $1 ...
-  docker exec $1 \
+  docker exec -e USER=root $1 \
   bash -c "curl -s -k -L -o - https://sparrowhub.org/bootstrap.sh | \
   bash && sparrow index update && sparrow box run $duckyfile"
 else
   echo run docker container from $1 ...
   #set -x
-  docker run  -v $PWD:/var/ducky -it --entrypoint="" $1 \
+  docker run  -e USER=root -v $PWD:/var/ducky -it --entrypoint="" $1 \
   bash -c "curl -s -k -L -o - https://sparrowhub.org/bootstrap.sh | \
   bash && sparrow index update && sparrow box run $duckyfile"
 fi
