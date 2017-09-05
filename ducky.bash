792c2b3680614b1f99408f7070e1e83f806ffe51
Alexey Melezhik <melezhik@gmail.com>
2017-09-05 13:46:00 +0300

    set format

--- ducky.bash
+++ ducky.bash
@@ -4,6 +4,12 @@ else
   duckyfile="/var/ducky/$DUCKYFILE"
 fi
 
+if test -z $OUTTHENTIC_FORMAT; then
+  format=production
+else
+  format=$OUTTHENTIC_FORMAT
+fi
+
 workdir=$(dirname "$duckyfile")
 
 echo run ducky from $duckyfile ...
@@ -14,10 +20,10 @@ if docker ps -f name=$1 | grep -q -w $1; then
   if test -z $DUCKY_SKIP_BOOTSTRAP; then
     docker exec -e USER=ROOT $1 \
     bash -c "curl -s -k -L -o - https://sparrowhub.org/bootstrap.sh | \
-    bash && export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format production"
+    bash && export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format $format"
   else
     docker exec -e USER=ROOT $1 \
-    bash -c " export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format production"
+    bash -c " export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format $format"
   fi
 
 else
@@ -27,10 +33,10 @@ else
   if test -z $DUCKY_SKIP_BOOTSTRAP; then
     docker run -e USER=ROOT -v $PWD:/var/ducky -it --entrypoint="" $1 \
     bash -c "curl -s -k -L -o - https://sparrowhub.org/bootstrap.sh | \
-    bash && export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format production"
+    bash && export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format $format"
   else
     docker run -e USER=ROOT -v $PWD:/var/ducky -it --entrypoint="" $1 \
-    bash -c " export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format production"  
+    bash -c " export OUTTHENTIC_CWD=$workdir && sparrow index update && sparrow box run $duckyfile --format $format"  
   fi   
 
 fi
